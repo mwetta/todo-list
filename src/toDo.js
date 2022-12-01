ad4452d7385b2749782efdc27cf1a58831499b03
Madeline <moon.madeline@gmail.com>
2022-12-01 10:26:22 -0600

    Format toDo to match Project

--- src/toDo.js
+++ src/toDo.js
@@ -1,22 +1,38 @@
 import { v4 as uuidv4 } from 'uuid';
 
-const toDo = (title, dueDate, description, priority, notes) => {
-    let id = uuidv4;
-    let toDoId = id + '_todo';
-    let dateCreated = Date();
+const toDo = (name, dueDate, description, priority, notes, project) => {
+    let id = uuidv4();
+    let date = format(new Date(), 'MM/dd/yyyy')
 
-    const getId = () => toDoId;
-    const getDateCreated = () => dateCreated;
-    const getTitle = () => title;
+    const getId = () => id;
+    const getDateCreated = () => date;
+    const getName = () => name;
     const getDueDate = () => dueDate;
     const getDescription = () => description;
     const getPriority = () => priority;
     const getNotes = () => notes;
+    const getProject = () => {
+        if (project === undefined) { return 'default'}
+        else { 
+            // need to reference projectId through UI and reference list
+        };}
+    const getToDo = () => 
+    `{ toDoId: '${id}', 
+    toDoCreationDate: ${date},
+    toDoName: '${name}', 
+    toDoDueDate: ${dueDate}, 
+    toDoDescription: '${description}', 
+    toDoPriority: '${priority}',
+    toDoNotes: '${notes}',
+    toDoProject: '${notes},
+     }`;
+
+    
 
         return {
             getId,
             getDateCreated,
-            getTitle,
+            getName,
             getDueDate,
             getDescription,
             getPriority,
