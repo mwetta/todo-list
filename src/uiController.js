c6f87a8acd314d2880ff3fcfa9ca397a71f487c6
Madeline <moon.madeline@gmail.com>
2022-12-14 11:31:14 -0600

    Write Default currently set to projects

--- src/uiController.js
+++ src/uiController.js
@@ -9,6 +9,25 @@ const uiController = (() => {
         let menu = document.createElement('div');
         menu.setAttribute('id', 'menu');
         content.appendChild(menu);
+        writeMain(content);
+    }
+
+    const writeMain = (content) => {
+        let main = document.createElement('div');
+        main.setAttribute('id', 'main');
+        content.appendChild(main);
+        writeDefault(main);
+    }
+
+    //currently set at default to show projects; ideal is to show upcoming tasks
+    const writeDefault = (main) => {
+        let projects = projectListController.getProjectList();
+        projects.forEach(project => {
+            let projectDiv = document.createElement('div');
+            let id = project.getId();
+            projectDiv.setAttribute('id', `${id}`);
+            main.appendChild(projectDiv);
+            })
     }
 
     const rewriteContent = () => {
@@ -21,6 +40,14 @@ const uiController = (() => {
 
     const writeProject = () => {
 
+    }
+
+    const writeAddForm = (element) => {
+        // if element = project 
+            // call add project 
+            // associate form fields with each part
+        // if element = todo
+            // 
     }
 
 return {writeMenu}
